6fec643f77abc326ba9dee703f46d0607472e86c
alessandro-massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-05-03 11:15:31 +0200

    added watering response

--- conservatoriumotica/conservatoriumotica.ino
+++ conservatoriumotica/conservatoriumotica.ino
@@ -2,22 +2,33 @@ int l = 0;  //Current level of the tank
 int M = 80; //Maximum level of the tank
 int m = 20; //Minimum level of the tank
 
-int discharge = ; //Discharge valve control pin
-int charge = ; //Charge valve control pin
+int discharge = 2; //Discharge valve control pin
+int charge = 3;    //Charge valve control pin
+int watering = 4;
 
-void setup() {
-  // put your setup code here, to run once:
-   pinMode(discharge, OUTPUT);
-   pinMode(charge,OUTPUT);
+void setup()
+{
+    // setup
+    pinMode(discharge, OUTPUT);
+    pinMode(charge, OUTPUT);
 }
 
-void loop() {
-  if(l>M){
-    digitalWrite(discharge,HIGH);
-  }else if(l<m){
-    digitalWrite(charge,HIGH);
-  }else{
-    digitalWrite(discharge,HIGH);
-    digitalWrite(charge,HIGH);
-  }
+void loop()
+{
+    // Main
+    if (l > M)
+    {
+        digitalWrite(discharge, HIGH);
+    }
+    else if (l < m)
+    {
+        digitalWrite(charge, HIGH);
+    }
+    else
+    {
+        digitalWrite(discharge, HIGH);
+        digitalWrite(charge, HIGH);
+    }
+
+    digitalRead(HIGH) ? digitalWrite(watering, HIGH) : digitalWrite(watering, LOW);
 }
