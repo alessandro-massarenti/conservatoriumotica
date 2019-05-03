4fd6fbcccf99448f4b34f489442eada46759bd2f
alessandro-massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-05-03 11:29:58 +0200

    Perfectioned variable naming

--- conservatoriumotica/conservatoriumotica.ino
+++ conservatoriumotica/conservatoriumotica.ino
@@ -2,33 +2,23 @@ int l = 0;  //Current level of the tank
 int M = 80; //Maximum level of the tank
 int m = 20; //Minimum level of the tank
 
-int discharge = 2; //Discharge valve control pin
-int charge = 3;    //Charge valve control pin
-int watering = 4;
+int euv = 2; //Emergency unload valve control pin
+int lv = 3;  //load valve control pin
+int uv = 4;  //Watering unload valve
+
+bool watering = false;
 
 void setup()
 {
     // setup
-    pinMode(discharge, OUTPUT);
-    pinMode(charge, OUTPUT);
+    pinMode(euv, OUTPUT);
+    pinMode(lv, OUTPUT);
 }
 
 void loop()
 {
     // Main
-    if (l > M)
-    {
-        digitalWrite(discharge, HIGH);
-    }
-    else if (l < m)
-    {
-        digitalWrite(charge, HIGH);
-    }
-    else
-    {
-        digitalWrite(discharge, HIGH);
-        digitalWrite(charge, HIGH);
-    }
-
-    digitalRead(HIGH) ? digitalWrite(watering, HIGH) : digitalWrite(watering, LOW);
+    (l > M) ? digitalWrite(euv, HIGH) : digitalWrite(euv, LOW);
+    (l < m) ? digitalWrite(lv, HIGH) : digitalWrite(lv, LOW);
+    watering ? digitalWrite(uv, HIGH) : digitalWrite(uv, LOW);
 }
