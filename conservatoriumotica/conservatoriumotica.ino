06cde223dd86c45665f009e1f53c20848e7043da
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-05-05 22:14:21 +0200

    Aggiunto sensore umidità del suolo
    Ogni 10 secondi testa il terreno e se è asciutto lo bagna per 1,5 secondi.
    
    N.B.:
    -L' arduino fa multitasking anche se è da migliorare.

--- conservatoriumotica/conservatoriumotica.ino
+++ conservatoriumotica/conservatoriumotica.ino
@@ -1,3 +1,10 @@
+
+int sensorValue = 0; // humidity default value
+float Vin = 5;       // Input voltage
+float Vout = 0;      // Vout default value
+float Rref = 47000;  // Reference resistor's value in ohms (you can give this value in kiloohms or megaohms - the resistance of the tested resistor will be given in the same units)
+float R = 0;
+
 // Tank life parameters
 int M = 80; //Maximum water level of the tank
 int m = 20; //Minimum water level of the tank
@@ -7,42 +14,59 @@ int l = 0;  //Current water level of the tank
 int h = 0; //Current humidity level in greenhouse
 
 // Valve control pins
-int EU_valve = 2; //Emergency unload valve control pin
+int EU_valve = 4; //Emergency unload valve control pin
 int L_valve = 3;  //load valve control pin
-int U_valve = 4;  //Watering unload valve
+int U_valve = 2;  //Watering unload valve
 
 // Sensor pins
-int TL_sensor = A0;
-int H_sensor = A1;
+int TL_sensor = A1;      //Tank level
+const int humidity = A0; // Analog input pin that senses Vout
 
 // Auxiliary
 int l1;
 int h1;
-bool watering = false;
+unsigned long t1 = 0;
 
 void setup()
 {
     // setup
+    Serial.begin(9600); // Initialize serial communications at 9600 bps
     pinMode(EU_valve, OUTPUT);
     pinMode(L_valve, OUTPUT);
+    pinMode(U_valve, OUTPUT);
 }
 
 void loop()
 {
+    unsigned long time_now = millis();
+    
+
     //Tank
-    l = analogRead(TL_sensor);
+    l = map(analogRead(TL_sensor), 0, 1023, 0, 100);
+    Serial.print("Level: ");
+    Serial.println(l); // Give calculated resistance in Serial Monitor
     if (l != l1)
     {
-        (l > M) ? digitalWrite(EU_valve, HIGH) : digitalWrite(EU_valve, LOW);
-        (l < m) ? digitalWrite(L_valve, HIGH) : digitalWrite(L_valve, LOW);
+        (l > M) ? digitalWrite(EU_valve, LOW) : digitalWrite(EU_valve, HIGH);
+        (l < m) ? digitalWrite(L_valve, LOW) : digitalWrite(L_valve, HIGH);
     }
     l1 = l;
 
-    h = analogRead(H_sensor);
-    if (h != h1)
+    if ((time_now - t1) >= 10000)
     {
-        
+        unsigned long time = millis();
+        sensorValue = analogRead(humidity);  // Read Vout on analog input pin A0 (Arduino can sense from 0-1023, 1023 is 5V)
+        Vout = (Vin * sensorValue) / 1023;   // Convert Vout to volts
+        R = Rref * (1 / ((Vin / Vout) - 1)); // Formula to calculate tested resistor's value
+        Serial.print("R: ");
+        Serial.println(R); // Give calculated resistance in Serial Monitor
+        if (R > 50000)
+        {
+            digitalWrite(2, LOW);
+            delay(1500);
+        }
+        digitalWrite(2, HIGH);
+        t1 = millis();
     }
-
-    watering ? digitalWrite(U_valve, HIGH) : digitalWrite(U_valve, LOW);
+    delay(1);
 }
